e3f67b7b6f2e3e1555a314d2a2bf0be9638c40ff
manthanPatel-android <manthanpatel@callhippo.com>
2021-01-25 16:20:35 +0530

    initial commit

---
+++ app/src/main/java/com/example/logutilsexample/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.logutilsexample;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+
+import com.example.callhippo_sdk_demo.logutils;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        logutils.logutils_method();
+    }
+}
---
+++ callhippo_sdk_demo/src/main/java/com/example/callhippo_sdk_demo/logutils.java
@@ -0,0 +1,13 @@
+package com.example.callhippo_sdk_demo;
+
+import android.nfc.Tag;
+import android.util.Log;
+
+public class logutils {
+
+   public static String TAG="logutils";
+
+    public static void logutils_method(){
+        Log.e(TAG,"manthan");
+    }
+}
